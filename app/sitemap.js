6805fb27ca1f4c50bcdf9b144121470f5aa11f2c
cyrilmwalimuke <cyrilmwalimuke@gmail.com>
2025-07-13 19:14:46 +0300

    random commit

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,19 +1,32 @@
 export default async function sitemap() {
     // const baseUrl = "https://jobske.com";
-    const baseUrl = "http://localhost:3000";
+    const baseUrl = "https://jobske.com";
     
 
-   const response =await fetch(`${baseUrl}/api/find-jobs`, {
+   const jobsResponse =await fetch(`${baseUrl}/api/find-jobs`, {
     next: { revalidate: 60 },
     method: 'GET',
   });
 
-    const jobs = await response.json();
+    const jobs = await jobsResponse.json();
 
     const postEntries = jobs.map(job => ({
       url: `${baseUrl}/job/${job.slug}`,
         lastModified: new Date(job.updatedAt),
     }));
+
+   
+    const blogsResponse =await fetch(`${baseUrl}/api/get-blogs`, {
+        next: { revalidate: 60 },
+        method: 'GET',
+      });
+    const blogs = await blogsResponse.json();
+
+    const blogEntries = jobs.map(blog => ({
+        url: `${baseUrl}/blog/${blog.slug}`,
+          lastModified: new Date(blog.updatedAt),
+      }));
+  
 
 
 
@@ -24,6 +37,17 @@ export default async function sitemap() {
             lastModified: new Date(),   
         },
         ...postEntries,
+        ...blogEntries,
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+        },
+       
+
 
     ]
   }
