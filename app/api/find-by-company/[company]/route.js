32980893af960d97240d5ccd7f7632c35abc153f
cyrilmwalimuke <cyrilmwalimuke@gmail.com>
2025-08-21 16:25:12 +0300

    random commit

--- app/api/find-by-company/[company]/route.js
+++ app/api/find-by-company/[company]/route.js
@@ -12,7 +12,7 @@ export async function GET(request, { params }) {
     await dbConnect();
 
     try {
-      const jobs = await Job.find({ employer_name: new RegExp(company, 'i') }); // case-insensitive match
+      const jobs = await Job.find({ employer_name: new RegExp(company, 'i') }).sort({ createdAt: -1 }); // case-insensitive match
       return NextResponse.json(jobs, { status: 200 });
     } catch (error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
